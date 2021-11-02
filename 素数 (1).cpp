4238013f0be6b3b72c979730c32d2838d61e2847
Duckyson <90754123+Duckyson@users.noreply.github.com>
2021-11-02 23:47:00 +0800

    zuoye

---
+++ 素数 (1).cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
+bool sushu(int n){
+	int i;
+	if(n==2||n==3){
+	return true;
+	}
+	for(i=2;i*i<=n;i++){
+		if(n%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+int main(){
+	unsigned int Y;
+	unsigned int p=1,q=1;
+	long long unsigned int a,b;
+    for(Y=2;Y<=10000;Y++) {
+    	if(sushu(Y)==true){
+    		for(p=1;p<=13;p++){
+    			for(q=1;q<=13;q++){
+    				if(Y==pow(p,q)+pow(q,p))
+    				printf("%d %-3d",p,q);
+    				else continue;
+				}
+			}
+		}
+	}
+	return 0;
+}
